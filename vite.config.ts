3d69a099ba71f4066dce5e4f5af9d8eab7f480c8
LakshyaReddy <147639048+LakshyaReddy@users.noreply.github.com>
2025-11-13 10:46:24 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as process from 'process'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
+    server: {
+      port: 3000
+    }
+  }
+})
